f2bb1ddf8f775f53054ec160751a94ab1a0778f7
Sanorian <krajnov.sascha@yandex.ru>
2023-06-27 10:59:00 +0400

    Update 7 functions to 2

--- script.js
+++ script.js
@@ -1,60 +1,26 @@
 let clicks_per_click=1, click_count=0, auto_click=0;
+
 function makeClick(){
     click_count+=clicks_per_click;
+    
 }
-function buy3Clicks(){
-    if (click_count>=60){
-        clicks_per_click+=3;
-        click_count-=60;
+function buying_some_clicks(count_of_buying_clicks){
+    if (click_count>=20*count_of_buying_clicks){
+        clicks_per_click+=count_of_buying_clicks;
+        click_count-=20*count_of_buying_clicks;
     }else{
-        alert('You are missing '+(60-click_count)+' clicks');
+        alert('You are missing '+(20*count_of_buying_clicks-click_count)+' clicks');
     }
 }
-function buy5Clicks(){
-    if (click_count>=100){
-        clicks_per_click+=5;
-        click_count-=100;
+function buying_some_automatise_clicks(count_of_buying_automatise_clicks){
+    if (click_count>=100*count_of_buying_automatise_clicks){
+        auto_click+=count_of_buying_automatise_clicks;
+        click_count-=100*count_of_buying_automatise_clicks;
     }else{
-        alert('You are missing '+(100-click_count)+' clicks');
+        alert('You are missing '+(100*count_of_buying_automatise_clicks-click_count)+' clicks');
     }
 }
-function buy10Clicks(){
-    if (click_count>=200){
-        clicks_per_click+=10;
-        click_count-=200;
-    }else{
-        alert('You are missing '+(200-click_count)+' clicks');
-    }
-}
-function buy100Clicks(){
-    if (click_count>=2000){
-        clicks_per_click+=100;
-        click_count-=2000;
-    }else{
-        alert('You are missing '+(2000-click_count)+' clicks');
-    }
-}
-function buy1automise_click(){
-    if (click_count>=100){
-        auto_click+=1;
-        click_count-=100;
-    }else{
-        alert('You are missing '+(100-click_count)+' clicks');
-    }}
-    function buy10automise_click(){
-        if (click_count>=1000){
-            auto_click+=10;
-            click_count-=1000;
-        }else{
-            alert('You are missing '+(1000-click_count)+' clicks');
-        }}
-        function buy100automise_click(){
-            if (click_count>=10000){
-                auto_click+=100;
-                click_count-=10000;
-            }else{
-                alert('You are missing '+(10000-click_count)+' clicks');
-            }}
+
 var click_text = document.getElementById('mydiv');
 setInterval(function(){
     click_text.innerHTML='<h2>Your count of clicks is '+click_count+'</h2>';
